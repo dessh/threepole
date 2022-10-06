2dcaf73c08172951e0601f5815c9508069f73325
dessh <93358231+dessh@users.noreply.github.com>
2022-10-06 18:39:43 +1300

    Switch to DOMContentLoaded

--- src/svelte/Window.svelte
+++ src/svelte/Window.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { appWindow } from "@tauri-apps/api/window";
 
-    window.addEventListener("load", () => {
+    window.addEventListener("DOMContentLoaded", () => {
         appWindow.show();
         appWindow.setFocus();
     });
