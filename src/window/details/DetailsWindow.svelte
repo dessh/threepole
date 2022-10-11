82dba53302f9487f5945686d06be0a1534862d68
dessh <93358231+dessh@users.noreply.github.com>
2022-10-12 04:37:07 +1300

    Add welcome banner

--- src/window/details/DetailsWindow.svelte
+++ src/window/details/DetailsWindow.svelte
@@ -23,6 +23,7 @@
     let playerData: PlayerData;
     let error: string;
     $: countedClears = playerData ? countClears(playerData.activityHistory) : 0;
+    let showBanner = false;
 
     let activityInfoMap: { [hash: number]: ActivityInfo } = {};
 
@@ -86,6 +87,28 @@
 
 <main>
     {#if playerData || error}
+        {#if showBanner}
+            <div class="banner margin">
+                <div class="text">
+                    <p class="title">Welcome to threepole!</p>
+                    <p>
+                        The overlay is disabled by default, and can be enabled
+                        in preferences.
+                    </p>
+                </div>
+                <button on:click={() => (showBanner = false)}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="20"
+                        width="20"
+                    >
+                        <path
+                            d="M6.062 15 5 13.938 8.938 10 5 6.062 6.062 5 10 8.938 13.938 5 15 6.062 11.062 10 15 13.938 13.938 15 10 11.062Z"
+                        />
+                    </svg>
+                </button>
+            </div>
+        {/if}
         <div class="header margin">
             <div class="status">
                 {#if playerData}
@@ -188,6 +211,42 @@
 
     .margin {
         margin: 24px 48px;
+    }
+
+    .banner {
+        display: flex;
+        padding: 12px 20px;
+        background: linear-gradient(
+            45deg,
+            var(--secondary-highlight-light),
+            var(--primary-highlight-light)
+        );
+        align-items: center;
+    }
+
+    .banner .text {
+        flex: 1;
+        font-weight: 300;
+        font-size: 14px;
+    }
+
+    .banner .text .title {
+        margin-bottom: 6px;
+        font-weight: 400;
+        font-size: 16px;
+    }
+
+    .banner button {
+        width: 24px;
+        height: 24px;
+        margin-left: 8px;
+        fill: #fff;
+        font-size: 0;
+        transition: background-color 0.1s;
+    }
+
+    .banner button:hover {
+        background-color: rgba(0, 0, 0, 0.2);
     }
 
     .grey {
